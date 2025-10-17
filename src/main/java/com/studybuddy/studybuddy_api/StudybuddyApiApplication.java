83a0acd0042e4696278e03ea89c024bc7d46bd44
Ester <ester.hb@hotmail.com>
2025-10-17 16:58:10 -0300

    primeiro commit, estrutura inicial da API

---
+++ src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddy.studybuddy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybuddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudybuddyApiApplication.class, args);
+	}
+
+}
