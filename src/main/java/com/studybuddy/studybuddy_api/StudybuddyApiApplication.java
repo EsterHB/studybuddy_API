b9f9cd9c80ec6831f088316c07b29e167f09e978
Ester <ester.hb@hotmail.com>
2025-10-22 19:53:34 -0300

    dependencia H2 removida do pom.xml

--- src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
+++ src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
@@ -1,13 +1,23 @@
 package com.studybuddy.studybuddy_api;
 
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudybuddyApiApplication {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudybuddyApiApplication.class, args);
 	}
+
+    @PostConstruct
+    public void logDatabaseUrl() {
+        System.out.println("🔍 URL do banco carregada: " + dbUrl);
+    }
 
 }
