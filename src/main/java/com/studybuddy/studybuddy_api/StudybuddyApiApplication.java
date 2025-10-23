95130842eeeaf01993bc6578429165f396df5ea6
Ester <ester.hb@hotmail.com>
2025-10-23 09:54:18 -0300

    correção de testes, exportação de variaveis antes de testes

--- src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
+++ src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
@@ -1,23 +1,30 @@
 package com.studybuddy.studybuddy_api;
 
 import jakarta.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudybuddyApiApplication {
+
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudybuddyApiApplication.class, args);
-	}
+    @Value("${spring.datasource.username:sa}")
+    private String dbUser;
 
-    @PostConstruct
-    public void logDatabaseUrl() {
-        System.out.println("🔍 URL do banco carregada: " + dbUrl);
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudybuddyApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void logDatabaseConfig() {
+        System.out.println("🔍 Tipo de driver: " + driver);
+        System.out.println("🔍 URL do banco: " + dbUrl);
+        System.out.println("🔍 Usuário: " + dbUser);
+    }
 }
